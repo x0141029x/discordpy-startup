f472f2896ff0743577fb204ef101c4dd83266c65
rintyanx <58331533+rintyanx@users.noreply.github.com>
2019-11-29 22:17:44 +0900

    Add files via upload

---
+++ login.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='$')
+
+
+@bot.command(name="ミミッキュ")
+async def mimikkyu(ctx):
+    await ctx.send(f"H:55 A:90 B:80 C:50 D:105 S:96")
+
+
+@bot.command(name="さようなら")
+async def goodbye(ctx):
+    await ctx.send(f"じゃあね、{ctx.message.author.name}さん！")
+
+
+bot.run('NjQ5NjQwMDE4NjY2ODQ4Mjcx.XeEZ0g.ySH_0H9jnnXNkU64FWH2IA2YamQ')
