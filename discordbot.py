137120e1f2fd86623e6e90bafa171fdde6422928
rintyanx <58331533+rintyanx@users.noreply.github.com>
2019-11-29 20:59:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,17 @@
+# coding: utf-8
 from discord.ext import commands
-import os
-import traceback
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+bot = commands.Bot(command_prefix='$')
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@bot.command(name="ミミッキュ")
+async def mimikkyu(ctx):
+    await ctx.send(f"H:55 A:90 B:80 C:50 D:105 S:96")
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@bot.command(name="さようなら")
+async def goodbye(ctx):
+    await ctx.send(f"じゃあね、{ctx.message.author.name}さん！")
 
 
-bot.run(token)
+bot.run('NjQ5NjQwMDE4NjY2ODQ4Mjcx.XeCC4w.7yDQUVWr8Epi2D5TbEv7iltrfIA')
